d63c90632e2f90885178b49e03641461e720758b
moeyua <moeyua@foxmail.com>
2022-06-21 14:36:26 +0800

    rebuild

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/expect -f
+set file dist
+set serverPath ~/apps/sub-web
+# 改为服务器用户名
+set user root
+# ip改为服务器ip
+set host 43.142.138.244
+# 改为服务器密码
+set password fwd!uSkeT5t*Vp
+set timeout -1
+spawn scp -r $file $user@$host:$serverPath
+expect "*password:*"
+send "$password\r"
+expect eof
